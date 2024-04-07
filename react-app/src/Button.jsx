e7e60f5524a3bfbed8b2da9cf47644b372cb071a
trungdung70199 <trungdung70199@gmail.com>
2024-04-08 01:43:59 +0900

    update button

--- react-app/src/Button.jsx
+++ react-app/src/Button.jsx
@@ -1,7 +1,17 @@
 function Button() {
-    return(
-        <button className="button">Click me</button>
-    );
+  // Use inline
+  // const styles = {
+  //     backgroundColor: "hsl(200, 100%, 50%)",
+  //     color: "white",
+  //     padding: "10px 20px",
+  //     borderRadius: "5px",
+  //     border: "none",
+  //     cursor: "pointer",
+  // }
+  return (
+    // <button className={styles}>Click me</button>
+    <button className="button">Click me</button>
+  );
 }
 
-export default Button
+export default Button;
