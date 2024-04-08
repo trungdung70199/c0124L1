50cf7f14efd3af5c364c247eacfda26bd7684037
trungdung70199 <trungdung70199@gmail.com>
2024-04-08 21:45:29 +0900

    add student

---
+++ react-app/src/Student.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+function Student(props) {
+  return (
+    <div className="student">
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+      <p>Student: {props.isStudent ? "Yes" : "No"}</p>
+    </div>
+  );
+}
+
+export default Student;
